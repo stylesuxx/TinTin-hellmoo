bf7c3dd6603b05c5ea4a65a89f89bb1050f6df2e
stylesuxx <stylesuxx@gmail.com>
2014-11-05 21:46:13 +0100

    Set terminator title to HellMoo

--- hellmoo.sh
+++ hellmoo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start terminator with the hellmo layout.
 # If you want to start terminator in full screen, add the --fullscreen flag
-terminator --layout hellmoo -m
+terminator --layout hellmoo -m --title HellMoo
