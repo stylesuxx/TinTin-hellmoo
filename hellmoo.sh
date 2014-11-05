bef79ca6cfd2b7ff4186dbec085084e7878ba6a4
stylesuxx <stylesuxx@gmail.com>
2014-11-05 21:56:41 +0100

    Use provided terminator config. Adapted path to relative.

--- hellmoo.sh
+++ hellmoo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Start terminator with the hellmo layout.
 # If you want to start terminator in full screen, add the --fullscreen flag
-terminator --layout hellmoo -m --title HellMoo
+terminator --config ./terminator.config --maximise --title HellMoo
